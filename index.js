855dab0e0cfaef86d8a3001bbf788c510d21d1e7
Ollie Monk <ollie@olliemonk.com>
2022-05-27 08:08:11 +0100

    Add retention days option

--- index.js
+++ index.js
@@ -3,12 +3,13 @@ const { upload } = require('docker-image-artifact');
 const githubActionIO = require('./actions_io');
 
 const INPUT_IMAGE = 'image';
-
+const RETENTION_DAYS= 'retention_days'
 const OUTPUT_ARTIFACT_NAME = 'artifact_name';
 
 async function runAction(getInput, writeOutput) {
     const imageName = getInput(INPUT_IMAGE, true);
-    const artifactName = await upload(imageName);
+    const retentionDays = getInput(RETENTION_DAYS, false);
+    const artifactName = await upload(imageName, retentionDays);
     writeOutput(OUTPUT_ARTIFACT_NAME, artifactName);
 }
 
