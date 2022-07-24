0139971848a26a97d72193b4700ad8c7afd3d3db
Ishwor Khadka <ishworkh@gmail.com>
2022-07-24 17:29:28 +0300

    Add e2e github workflow

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ const OUTPUT_ARTIFACT_NAME = 'artifact_name';
 
 async function runAction(getInput, writeOutput) {
     const imageName = getInput(INPUT_IMAGE, true);
+
     const artifactName = await upload(imageName);
     writeOutput(OUTPUT_ARTIFACT_NAME, artifactName);
 }
 
 runAction(githubActionIO.getInput, githubActionIO.writeOutput)
-    .then(() => console.log("Uploading finished") )
+    .then(() => console.log("Uploading finished"))
     .catch((err) => githubActionIO.fail(err));
-    
+
 
